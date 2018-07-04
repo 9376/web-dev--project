aadc4b14c10b76e37a000b8fb5f12dd16126fff9
Obabo <Obabo@users.github.com>
2018-07-04 22:44:26 +0300

    coolio

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <style media="screen">
+      #center{
+color: white;
+        background-color:#CC376E;
+        width:500px;
+        height: 500px;
+
+      }
+    </style>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="center">
+      this is the best sign up 
+    </div>
+  </body>
+</html>
